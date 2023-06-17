2406832dfc8e07c17c0711e7982ba6eb0c36374d
ARIBİLGİ <ARIBLG@PC102E>
2023-06-17 10:15:33 +0300

    17-06-2023 v1

---
+++ MVCEgitim/Controllers/MVC08ViewResultsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEgitim.Controllers
+{
+    public class MVC08ViewResultsController : Controller
+    {
+        // GET: MVC08ViewResults
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
